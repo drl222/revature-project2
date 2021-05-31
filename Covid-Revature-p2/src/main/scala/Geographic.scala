7fc34a89eedf79145ab2fc9e3bf0e18f447ddc5e
Derrick Lin <drl222@cornell.edu>
2021-05-31 17:41:02 -0400

    geographical regions redefined

--- Covid-Revature-p2/src/main/scala/Geographic.scala
+++ Covid-Revature-p2/src/main/scala/Geographic.scala
@@ -13,14 +13,13 @@ object Geographic {
 
     val data3 = spark.sql("create or replace TEMPORARY view world_view as select count(w.`Country/Region`) as Num_of_Countries, " +
       "sum(cast(w.`5/2/21` as int)) as Confirmed_Cases, sum(cast(d.`5/2/21` as int)) as Deaths, case " +
-      "when ((w.Lat between -90 and 0) AND (w.Long between 90 and 180)) then '(-90,0) & (90,180)' " +
-      "when ((w.Lat between 15 and 50) AND (w.Long between 85 and 140)) then '(15,50) & (85,140)'" +
-      "when ((w.Lat between 20 and 60) AND (w.Long between 0 and 80)) then '(20,60) & (0,80)'" +
-      "when ((w.Lat between -40 and 40) AND (w.Long between -120 and 0)) then '(-40,40) & (-120,0)'" +
-      "when ((w.Lat between -90 and 60) AND (w.Long between -90 and 20)) then '(-90,60) & (-90,20)'" +
-      "when ((w.Lat between 0 and 90) AND (w.Long between 0 and 90)) then '(0,90) & (0,90)'" +
-      "when ((w.Lat between -40 and 10) AND (w.Long between 20 and 120)) then '(-40,0) & (20,120)'" +
-      "when ((w.Lat between 40 and 60) AND (w.Long between -180 and -90)) then '(40,60) & (-90,-180)' " +
+      "when ((w.Lat between -90 and 0) AND (w.Long between 90 and 180)) then 'Australia/Pacific Islands' " +
+      "when ((w.Lat between 0 and 90) AND (w.Long between 90 and 180)) then 'East/Southeast Asia'" +
+      "when ((w.Lat between 40 and 90) AND (w.Long between -25 and 90)) then 'Europe'" +
+      "when ((w.Lat between 0 and 40) AND (w.Long between -25 and 90)) then 'North Africa/Middle East/South Asia'" +
+      "when ((w.Lat between -90 and 0) AND (w.Long between -25 and 90)) then 'Southern Africa'" +
+      "when ((w.Lat between -90 and 10) AND (w.Long between -180 and -25)) then 'South America'" +
+      "when ((w.Lat between 10 and 90) AND (w.Long between -180 and -25)) then 'North America'" +
       "else 'Not Labeled' END AS Latitude_and_Longitude from world as w join Deaths as d on " +
       "(w.`Country/Region` = d.`Country/Region` and w.`Province/State` IS NOT DISTINCT FROM d.`Province/State`) " +
       "group by `Latitude_and_Longitude` " +
